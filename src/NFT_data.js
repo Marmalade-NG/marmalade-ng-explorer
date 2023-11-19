44bfdc1699bf57ea16f93f8564644a5b3537960d
CryptoPascal31 <CryptoPascal31@users.noreply.github.com>
2023-11-19 20:55:47 +0100

    Improve image and ipfs handling

--- src/NFT_data.js
+++ src/NFT_data.js
@@ -1,14 +1,21 @@
 import { CID } from 'multiformats/cid'
+import delay from 'delay';
 import useSWRImmutable from 'swr/immutable'
 import EMPTY_IMG from './assets/empty.png'
+import MISSING_IMG from './assets/missing.png'
+
 
 const EMPTY_META = {name:"", description:""};
 const DEFAULT_DATA = {meta:null, img:EMPTY_IMG}
-
+const DEFAULT_MISSING = {meta:null, img:MISSING_IMG}
 
 
 const to_path_resolution = (gw, cid) => "https://" + [gw, "ipfs", cid].join("/");
 const to_subdom_resolution = (gw, cid) => "https://" + [cid, "ipfs", gw].join(".");
+
+
+const del_fetch = (d, sig, uri) => delay(d, {signal:sig})
+                                   .then(() => fetch(uri))
 
 function _fetch(uri)
 {
@@ -16,11 +23,14 @@ function _fetch(uri)
   if(protocol == "ipfs:")
   {
     const cid = CID.parse(_cid).toV1().toString()
+    const ctr = new AbortController();
 
-    return fetch(to_path_resolution("ipfs.io", cid), { signal: AbortSignal.timeout(1000)})
-           .catch(() => fetch(to_subdom_resolution("cf-ipfs.com", cid), { signal: AbortSignal.timeout(1000)}))
-           .catch(() => fetch(to_subdom_resolution("dweb.link", cid), { signal: AbortSignal.timeout(1000)}))
-           .catch(() => fetch(to_path_resolution("gateway.pinata.cloud", cid), { signal: AbortSignal.timeout(1000)}));
+    return Promise.any([ del_fetch(0, ctr.signal, to_subdom_resolution("dweb.link", cid)),
+                         del_fetch(2000, ctr.signal, to_path_resolution("ipfs.io", cid)),
+                         del_fetch(4000, ctr.signal, to_path_resolution("cloudflare-ipfs.com", cid)),
+                         del_fetch(6000, ctr.signal, to_subdom_resolution("cf-ipfs.com", cid)),
+                         del_fetch(8000, ctr.signal, to_path_resolution("gateway.pinata.cloud", cid))
+                       ]).then(x => {ctr.abort(); return x})
   }
   else
     return fetch(uri);
@@ -56,9 +66,8 @@ function fetchData(uri)
 function useNFTdata(uri)
 {
   const {data, error} = useSWRImmutable(uri, fetchData);
-  if(error)
-    console.warn(error);
-  return {data:data?data:DEFAULT_DATA, error}
+
+  return {data:data?data:(error?DEFAULT_MISSING:DEFAULT_DATA), error}
 }
 
 export {useNFTdata}
